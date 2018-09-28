407171eab6ef4172c08d7cf9ec9406000c4e8c36
dmazzella <damianomazzella@gmail.com>
2018-09-28 16:25:36 +0200

    fix sha

--- cryptoauthlib/basic.py
+++ cryptoauthlib/basic.py
@@ -18,6 +18,34 @@ class ATECCBasic(object):
             return ATCA_STATUS.ATCA_SUCCESS, "Success"
 
     ###########################################################################
+    #          CryptoAuthLib Basic API methods for CheckMAC command           #
+    ###########################################################################
+
+    ###########################################################################
+    #           CryptoAuthLib Basic API methods for Counter command           #
+    ###########################################################################
+
+    ###########################################################################
+    #          CryptoAuthLib Basic API methods for DeriveKey command          #
+    ###########################################################################
+
+    ###########################################################################
+    #            CryptoAuthLib Basic API methods for ECDH command             #
+    ###########################################################################
+
+    ###########################################################################
+    #           CryptoAuthLib Basic API methods for GenDig command            #
+    ###########################################################################
+
+    ###########################################################################
+    #           CryptoAuthLib Basic API methods for GenKey command            #
+    ###########################################################################
+
+    ###########################################################################
+    #            CryptoAuthLib Basic API methods for HMAC command             #
+    ###########################################################################
+
+    ###########################################################################
     #            CryptoAuthLib Basic API methods for Info command             #
     ###########################################################################
 
@@ -31,6 +59,50 @@ class ATECCBasic(object):
 
     def atcab_info(self):
         return self.atcab_info_base(ATCA_CONSTANTS.INFO_MODE_REVISION)
+
+    ###########################################################################
+    #             CryptoAuthLib Basic API methods for KDF command             #
+    ###########################################################################
+
+    ###########################################################################
+    #             CryptoAuthLib Basic API methods for Lock command            #
+    ###########################################################################
+
+    ###########################################################################
+    #             CryptoAuthLib Basic API methods for MAC command             #
+    ###########################################################################
+
+    ###########################################################################
+    #            CryptoAuthLib Basic API methods for Nonce command            #
+    ###########################################################################
+
+    ###########################################################################
+    #          CryptoAuthLib Basic API methods for PrivWrite command          #
+    ###########################################################################
+
+    ###########################################################################
+    #           CryptoAuthLib Basic API methods for Random command            #
+    ###########################################################################
+
+    def atcab_random(self):
+        packet = ATCAPacket(
+            opcode=ATCA_CONSTANTS.ATCA_RANDOM,
+            param1=ATCA_CONSTANTS.RANDOM_SEED_UPDATE,
+        )
+        self.execute(packet)
+        return packet
+
+    ###########################################################################
+    #             CryptoAuthLib Basic API methods for Read command            #
+    ###########################################################################
+
+    ###########################################################################
+    #          CryptoAuthLib Basic API methods for SecureBoot command         #
+    ###########################################################################
+
+    ###########################################################################
+    #           CryptoAuthLib Basic API methods for SelfTest command          #
+    ###########################################################################
 
     ###########################################################################
     #            CryptoAuthLib Basic API methods for SHA command              #
@@ -65,25 +137,33 @@ class ATECCBasic(object):
         return packet
 
     def atcab_sha(self, data):
-        SHA256_BLOCK_SIZE = ATCA_CONSTANTS.ATCA_SHA256_BLOCK_SIZE
-        data_mv = memoryview(data)
+        bs = ATCA_CONSTANTS.ATCA_SHA256_BLOCK_SIZE
+        d_mv = memoryview(data)
         packet = self.atcab_sha_base(ATCA_CONSTANTS.SHA_MODE_SHA256_START)
-        for i in range(0, len(data_mv), SHA256_BLOCK_SIZE):
-            b = data_mv[i:i + SHA256_BLOCK_SIZE]
+        for i in range(0, len(d_mv), bs):
+            lb = not len(d_mv) % bs
+            b = d_mv[i:i + bs]
             m = ATCA_CONSTANTS.SHA_MODE_SHA256_UPDATE
-            if len(b) < SHA256_BLOCK_SIZE:
+            if len(b) == bs:
+                packet = self.atcab_sha_base(m, b)
+            if len(b) < bs or lb:
+                b = b'' if lb else b
                 m = ATCA_CONSTANTS.SHA_MODE_SHA256_END
-            packet = self.atcab_sha_base(m, b)
+                packet = self.atcab_sha_base(m, b)
         return packet
 
     ###########################################################################
-    #           CryptoAuthLib Basic API methods for Random command            #
+    #            CryptoAuthLib Basic API methods for Sign command             #
     ###########################################################################
 
-    def atcab_random(self):
-        packet = ATCAPacket(
-            opcode=ATCA_CONSTANTS.ATCA_RANDOM,
-            param1=ATCA_CONSTANTS.RANDOM_SEED_UPDATE,
-        )
-        self.execute(packet)
-        return packet
+    ###########################################################################
+    #         CryptoAuthLib Basic API methods for UpdateExtra command         #
+    ###########################################################################
+
+    ###########################################################################
+    #            CryptoAuthLib Basic API methods for Verify command           #
+    ###########################################################################
+
+    ###########################################################################
+    #            CryptoAuthLib Basic API methods for Write command            #
+    ###########################################################################
