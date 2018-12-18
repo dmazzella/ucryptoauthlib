104a5e73e6bd7dc0f8b3401848a6d37b6bcee840
dmazzella <damianomazzella@gmail.com>
2018-12-18 12:57:47 +0100

    changed bytes to hexlify for assert output

--- tests/ateccX08a/tests_read.py
+++ tests/ateccX08a/tests_read.py
@@ -18,8 +18,8 @@ def run(device=None):
 
     packet = device.atcab_read_serial_number()
     sn0_1, sn8 = packet.response_data[1:1+2], packet.response_data[9+4:9+4+1]
-    assert b'\x01#' == sn0_1, bytes(sn0_1)
-    assert b'\xee' == sn8, bytes(sn8)
+    assert b'\x01#' == sn0_1, hexlify(sn0_1)
+    assert b'\xee' == sn8, hexlify(sn8)
     log.debug("atcab_read_serial_number: %s", hexlify(packet.response_data))
 
     packets = device.atcab_read_config_zone()
@@ -37,8 +37,8 @@ def run(device=None):
         locked = False
         if zone == LOCK_ZONE_CONFIG:
             zone_str = "LOCK_ZONE_CONFIG"
-            locked = (packet.response_data[3] != 0x55)
+            locked = bool(packet.response_data[3] != 0x55)
         elif zone == LOCK_ZONE_DATA:
             zone_str = "LOCK_ZONE_DATA"
-            locked = (packet.response_data[2] != 0x55)
+            locked = bool(packet.response_data[2] != 0x55)
         log.debug("atcab_is_locked: %s %r %s", zone_str, locked, hexlify(packet.response_data))
--- tests/ateccX08a/tests_sha.py
+++ tests/ateccX08a/tests_sha.py
@@ -19,7 +19,7 @@ def run(device=None):
     expected = b"\x1a:\xa5E\x04\x94S\xaf\xdf\x17\xe9\x89\xa4\x1f\xa0\x97\x94\xa5\x1b\xd5\xdb\x9167gU\x0c\x0f\n\xf3'\xd4"
     message = b'\xBC' * ATCA_SHA256_BLOCK_SIZE
     packet = device.atcab_sha(message)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha: %s", hexlify(packet.response_data))
 
     expected = b'p~\x97\xe6\xf8d]\xf5\xd8\x068.g\x01\xc8\xe2\xe2\x16`\x17\xf6\nV\xe6\xaa\xc0\xc2\xd2\xdb\xbb"\x81'
@@ -29,32 +29,32 @@ def run(device=None):
     packet = device.atcab_sha_base(SHA_MODE_SHA256_UPDATE, message)
     packet = device.atcab_sha_base(SHA_MODE_SHA256_UPDATE, message)
     packet = device.atcab_sha_base(SHA_MODE_SHA256_END)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha_base: %s", hexlify(packet.response_data))
 
     # test HW SHA with a long message > SHA block size and not an exact SHA block-size increment
     expected = b'\xa9"\x18VCp\xa0W\'?\xf4\x85\xa8\x07?2\xfc\x1f\x14\x12\xec\xa2\xe3\x0b\x81\xa8\x87v\x0ba1r'
     message = b'\xBC' * (ATCA_SHA256_BLOCK_SIZE + 63)
     packet = device.atcab_sha(message)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha %d: %s", len(message), hexlify(packet.response_data))
 
     # test HW SHA with a short message < SHA block size and not an exact SHA block-size increment
     expected = b'0?\xf8\xba@\xa2\x06\xe7\xa9P\x02\x1e\xf5\x10f\xd4\xa0\x01Tu2>\xe9\xf2J\xc8\xc9c)\x8f4\xce'
     message = b'\xBC' * 10
     packet = device.atcab_sha(message)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha %d: %s", len(message), hexlify(packet.response_data))
 
     # test NIST HW SHA
     expected = b'\xbax\x16\xbf\x8f\x01\xcf\xeaAA@\xde]\xae"#\xb0\x03a\xa3\x96\x17z\x9c\xb4\x10\xffa\xf2\x00\x15\xad'
     message = "abc"
     packet = device.atcab_sha(message)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha nist 1: %s", hexlify(packet.response_data))
 
     expected = b'$\x8dja\xd2\x068\xb8\xe5\xc0&\x93\x0c>`9\xa3<\xe4Yd\xff!g\xf6\xec\xed\xd4\x19\xdb\x06\xc1'
     message = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
     packet = device.atcab_sha(message)
-    assert expected in bytes(packet.response_data), bytes(packet.response_data)
+    assert expected in bytes(packet.response_data), hexlify(packet.response_data)
     log.debug("atcab_sha nist 2: %s", hexlify(packet.response_data))
