67703819bce94e507a5c1250ee7712b79983a36b
dmazzella <damianomazzella@gmail.com>
2018-12-18 12:58:02 +0100

    changed bytes to hexlify for assert output

--- tests/ateccX08a/tests_info.py
+++ tests/ateccX08a/tests_info.py
@@ -11,19 +11,19 @@ def run(device=None):
     if not device:
         raise ValueError("device")
 
-    expected = (
-        b'\x07\x00\x00P\x00\x03\x91',  # ATECC508A
-        b'\x07\x00\x00`\x02\x808'  # ATECC608A
-    )
+    expected = {
+        "ATECC508A": b'\x07\x00\x00P\x00\x03\x91',
+        "ATECC608A": b'\x07\x00\x00`\x02\x808'
+    }
     packet = device.atcab_info()
-    assert packet.response_data in expected, packet.response_data
+    assert packet.response_data == expected[device.device], hexlify(packet.response_data)
     log.debug("atcab_info: %s", hexlify(packet.response_data))
 
     packet = device.atcab_info_base(INFO_MODE_REVISION)
-    assert packet.response_data in expected, packet.response_data
+    assert packet.response_data == expected[device.device], hexlify(packet.response_data)
     log.debug("atcab_info_base - revision: %s", hexlify(packet.response_data))
 
     expected = b'\x07\x00\x00\x00\x00\x03\xad'
     packet = device.atcab_info_base(INFO_MODE_STATE)
-    assert expected == packet.response_data, packet.response_data
+    assert expected == packet.response_data, hexlify(packet.response_data)
     log.debug("atcab_info_base - state: %s", hexlify(packet.response_data))
