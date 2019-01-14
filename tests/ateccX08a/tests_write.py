a4b3d5a8af624c0be05fa5fd2f50e1705dee9b55
dmazzella <damianomazzella@gmail.com>
2019-01-14 18:58:05 +0100

    update tests_write

--- tests/ateccX08a/tests_write.py
+++ tests/ateccX08a/tests_write.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=E0401
 import logging
+import sys
+
+from ubinascii import hexlify
+
+from cryptoauthlib import util as ATEC_UTIL
+from cryptoauthlib import constant as ATCA_CONSTANTS
+from cryptoauthlib import exceptions as ATCA_EXCEPTIONS
+from cryptoauthlib import status as ATCA_STATUS
 
 log = logging.getLogger("ateccX08a.tests_write")
 
@@ -27,8 +36,36 @@ _TEST_CONFIG = {
     ])
 }
 
+_TEST_KEYS = {
+    "PRIVATE": bytes([
+        0XF3, 0XFC, 0XCC, 0X0D, 0X00, 0XD8, 0X03, 0X19, 0X54, 0XF9, 0X08, 0X64, 0XD4, 0X3C, 0X24, 0X7F,
+        0X4B, 0XF5, 0XF0, 0X66, 0X5C, 0X6B, 0X50, 0XCC, 0X17, 0X74, 0X9A, 0X27, 0XD1, 0XCF, 0X76, 0X64
+    ]),
+    "PUBLIC": bytes([
+        0X8D, 0X61, 0X7E, 0X65, 0XC9, 0X50, 0X8E, 0X64, 0XBC, 0XC5, 0X67, 0X3A, 0XC8, 0X2A, 0X67, 0X99,
+        0XDA, 0X3C, 0X14, 0X46, 0X68, 0X2C, 0X25, 0X8C, 0X46, 0X3F, 0XFF, 0XDF, 0X58, 0XDF, 0XD2, 0XFA,
+        0X3E, 0X6C, 0X37, 0X8B, 0X53, 0XD7, 0X95, 0XC4, 0XA4, 0XDF, 0XFB, 0X41, 0X99, 0XED, 0XD7, 0X86,
+        0X2F, 0X23, 0XAB, 0XAF, 0X02, 0X03, 0XB4, 0XB8, 0X91, 0X1B, 0XA0, 0X56, 0X99, 0X94, 0XE1, 0X01
+    ])
+}
+
 def run(device=None):
     if not device:
         raise ValueError("device")
 
-    log.debug("%s", _TEST_CONFIG[device.device])
+    config = memoryview(_TEST_CONFIG[device.device])
+
+    log.debug("test_config for %s dump_configuration: %s", device.device, hexlify(config))
+    ATEC_UTIL.dump_configuration(config, stream=sys.stderr)
+
+    # try:
+    #     device.atcab_write_config_zone(config)
+    # except ATCA_EXCEPTIONS.CryptoError as ce:
+    #     log.error("atcab_write_config_zone: %s", ce)
+
+    # public_key = memoryview(_TEST_KEYS["PUBLIC"])
+
+    # try:
+    #     device.atcab_write_pubkey(12, public_key)
+    # except ATCA_EXCEPTIONS.CryptoError as ce:
+    #     log.error("atcab_write_pubkey: %s", ce)
